496c57f14b6f2e6aae69e60f6675591906045e13
Nikolas Žilić <zilicnikolas@gmail.com>
2025-11-21 00:51:21 +0100

    Create and style template for task history component

--- src/pages/TaskHistory.tsx
+++ src/pages/TaskHistory.tsx
@@ -10,8 +10,25 @@ function TaskHistory(){
 
   return(
    <>
-    Some Text
-    <button onClick={goBack}>Go Back</button>
+    <div className="title-wrapper">
+       <h1 className="task-manager-title">
+          Task Manager App
+       </h1>
+     </div>
+    <div className="go-back-div">
+      <button className="go-back-button" onClick={goBack}> 
+        Go Home
+      </button>
+    </div>
+    <div className="task-history-div">
+      <h2 className="task-history-title">
+        Task History
+      </h2>
+      <div className="task-history-details">
+        Task 1 status: unfinished date  delete task<br/>
+        Task 2 status: finished datev delete task
+      </div>
+    </div> 
    </>
   )
 }
