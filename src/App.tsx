6451129a17294c7dc5f1f5b4d5261e0e5841f728
Nikolas Žilić <zilicnikolas@gmail.com>
2026-08-17 21:05:52 +0200

    Simplify App component and move task logic to HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -2,62 +2,13 @@ import './App.css'
 import HomePage from './pages/HomePage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TaskHistory from './pages/TaskHistory';
-import { useTasks } from './hooks/useTasks';
-import { useState } from 'react';
-import type { Task, TaskSource } from './types/taskType';
-import { moveTaskToHistoryApi } from './api/moveTaskToHistroryApi';
 
 function App(){
-  const [isLoading, setIsLoading] = useState <boolean>(false);
-  const { tasks: tasksToday, setTasks: setTasksToday, fetchTasks: fetchTasksToday } = 
-   useTasks('https://692488a63ad095fb8474968f.mockapi.io/tasks');
-  const { tasks: tasksTomorrow, setTasks: setTasksTomorrow, fetchTasks: fetchTasksTomorrow } = 
-   useTasks('https://692488a63ad095fb8474968f.mockapi.io/tasks-tomorrow');
-  const { tasks: tasksInTwoDays, setTasks: setTasksInTwoDays, fetchTasks: fetchTasksInTwoDays } = 
-   useTasks('https://69288e25b35b4ffc50161e2b.mockapi.io/tasks-in-two-days');
-
-  const moveTaskToHistory = async(taskId: string, source: TaskSource): Promise<void> => {
-    let tasks: Task[];
-    let setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
-    let url: string;
-
-    if(source === 'today'){
-      tasks = tasksToday;
-      setTasks = setTasksToday;
-      url = 'https://692488a63ad095fb8474968f.mockapi.io/tasks';
-    }else if(source === 'tomorrow'){
-      tasks = tasksTomorrow;
-      setTasks = setTasksTomorrow;
-      url = 'https://692488a63ad095fb8474968f.mockapi.io/tasks-tomorrow';
-    }else {
-      tasks = tasksInTwoDays;
-      setTasks = setTasksInTwoDays;
-      url = 'https://69288e25b35b4ffc50161e2b.mockapi.io/tasks-in-two-days';
-    }
-
-    const moveTask = tasks.find((foundTask: Task) => taskId === foundTask.id);
-    if(!moveTask) return;
-  
-    setIsLoading(true);
-  
-    try{
-     await moveTaskToHistoryApi({url, taskId, task: moveTask});
-     setTasks(prev => prev.filter((task: Task) => task.id !== taskId));
-    }
-    catch(error){
-      console.log('Could not move the task. Please try again later.', error);
-    }
-    finally{
-      setIsLoading(false);
-    }
-  };
-
+ 
   return (
     <BrowserRouter>
      <Routes>
-       <Route index element={<HomePage tasksToday={tasksToday} fetchTasksToday={fetchTasksToday} 
-         tasksTomorrow={tasksTomorrow} tasksInTwoDays={tasksInTwoDays} fetchTasksTomorrow={fetchTasksTomorrow}
-         fetchTasksInTwoDays={fetchTasksInTwoDays} moveTaskToHistory={moveTaskToHistory} isLoading={isLoading} />} />
+       <Route index element={<HomePage />} />
        <Route path='/task-history' element={<TaskHistory />} />
      </Routes>
     </BrowserRouter>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,25 +8,59 @@ import TomorrowsTaskSection from '../components/TaskList/TomorrowsTaskSection/To
 import InTwoDaysTaskSection from '../components/TaskList/InTwoDaysTaskSection/InTwoDaysTaskSection';
 import type { Task, TaskSource } from '../types/taskType';
 import type { TaskStatusType } from '../types/taskStatusType';
+import { useTasks } from '../hooks/useTasks';
+import { moveTaskToHistoryApi } from '../api/moveTaskToHistroryApi';
 
-type HomePageTask = {
-  tasksToday: Task[];
-  tasksTomorrow: Task[];
-  tasksInTwoDays: Task[];
-  fetchTasksToday: () => Promise<void>;
-  fetchTasksTomorrow: () => Promise<void>;
-  fetchTasksInTwoDays: () => Promise<void>;
-  moveTaskToHistory: (taskId: string, source: TaskSource) => Promise<void>
-  isLoading: boolean;
-};
-
-function HomePage({ moveTaskToHistory, isLoading, tasksToday, tasksTomorrow, tasksInTwoDays,
-  fetchTasksToday, fetchTasksTomorrow, fetchTasksInTwoDays }: HomePageTask ){
+function HomePage(){
   const navigate = useNavigate(); 
   const [isInstructions, setIsInstructions] = useState<boolean>(false);
   const [taskDetails, setTaskDetails] = useState <string | null>(null);
   const [taskStatus, setTaskStatus] = useState<Record<string, TaskStatusType>>({});
+  const [isLoading, setIsLoading] = useState <boolean>(false);
+  const { tasks: tasksToday, setTasks: setTasksToday, fetchTasks: fetchTasksToday } = 
+   useTasks('https://692488a63ad095fb8474968f.mockapi.io/tasks');
+  const { tasks: tasksTomorrow, setTasks: setTasksTomorrow, fetchTasks: fetchTasksTomorrow } = 
+   useTasks('https://692488a63ad095fb8474968f.mockapi.io/tasks-tomorrow');
+  const { tasks: tasksInTwoDays, setTasks: setTasksInTwoDays, fetchTasks: fetchTasksInTwoDays } = 
+   useTasks('https://69288e25b35b4ffc50161e2b.mockapi.io/tasks-in-two-days');
  
+   const moveTaskToHistory = async(taskId: string, source: TaskSource): Promise<void> => {
+    let tasks: Task[];
+    let setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+    let url: string;
+
+    if(source === 'today'){
+      tasks = tasksToday;
+      setTasks = setTasksToday;
+      url = 'https://692488a63ad095fb8474968f.mockapi.io/tasks';
+    }else if(source === 'tomorrow'){
+      tasks = tasksTomorrow;
+      setTasks = setTasksTomorrow;
+      url = 'https://692488a63ad095fb8474968f.mockapi.io/tasks-tomorrow';
+    }else {
+      tasks = tasksInTwoDays;
+      setTasks = setTasksInTwoDays;
+      url = 'https://69288e25b35b4ffc50161e2b.mockapi.io/tasks-in-two-days';
+    }
+
+    const moveTask = tasks.find((foundTask: Task) => taskId === foundTask.id);
+    if(!moveTask) return;
+  
+    setIsLoading(true);
+  
+    try{
+     await moveTaskToHistoryApi({url, taskId, task: moveTask});
+     setTasks(prev => prev.filter((task: Task) => task.id !== taskId));
+    }
+    catch(error){
+      console.log('Could not move the task. Please try again later.', error);
+    }
+    finally{
+      setIsLoading(false);
+    }
+  };
+
+
   const toggleInstructions = (): void => {
     setIsInstructions(prev => !prev);
   } 
